2fb12afa8b6f15922845383d0d6901e448762b86
Divya Mishra <divyam.vns@gmail.com>
2025-05-06 17:08:11 -0400

    Add environment setup script

---
+++ setup_env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export PATH=/home/software/sratoolkit.3.1.1-ubuntu64/bin:$PATH
+export PATH=/home/software/STAR-2.7.11b/bin/Linux_x86_64:$PATH
+export PATH=/home/software/minimap2-2.28:$PATH
+export PATH=/home/software/samtools-1.20:$PATH
+export PATH=/home/software/cufflinks-2.2.1.Linux_x86_64:$PATH
+export PATH=/home/software/psiclass:$PATH
+export PATH=/home/software/rmats-turbo:$PATH
